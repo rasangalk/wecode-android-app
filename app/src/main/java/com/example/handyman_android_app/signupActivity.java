0a2a6e24c675730223b069edab7fa6075fb03c9b
rasangalk <rasangalakshith38@gmail.com>
2021-09-21 14:25:53 +0530

    signup frontend implemented
    not connected to the firebase

---
+++ app/src/main/java/com/example/handyman_android_app/signupActivity.java
@@ -0,0 +1,31 @@
+package com.example.handyman_android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class signupActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        Spinner location_spinner = findViewById(R.id.r_spin_signup_location);
+        location_spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this,adapterView.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
