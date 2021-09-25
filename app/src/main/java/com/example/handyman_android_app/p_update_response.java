c065bab828f4194ba3e8cf5b6cbd98c54f87389d
ChanmithK <praveenkariyawasam0@gmail.com>
2021-09-25 17:10:52 +0530

    crud operation done.

--- app/src/main/java/com/example/handyman_android_app/p_update_response.java
+++ app/src/main/java/com/example/handyman_android_app/p_update_response.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,45 +18,36 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-public class p_view_response extends AppCompatActivity {
+public class p_update_response extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private static final String KEY_DESCRIPTION= "description";
     private static final String KEY_LOCATION= "location";
     private static final String KEY_TITLE = "title";
-    private static final String KEY_CATEGORY="category";
+    private static final String KEY_CATEGORY= "category";
 
-//    private Button button;
-
-    private TextView textViewDescription;
-    private TextView textViewLocation;
-    private TextView textViewTitle;
-    private TextView textViewCategory;
+    private EditText textViewDescription;
+    private Spinner textViewLocation;
+    private EditText textViewTitle;
+    private EditText textViewCategory;
 
     String userId= FirebaseAuth.getInstance().getCurrentUser().getUid();
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    // private DocumentReference docRef = db.collection("Users").document("RequestedServices");
 
     DocumentReference docRef=FirebaseFirestore.getInstance().collection("Users")
             .document(userId).collection("Responses").document("210925.140139");
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pview_response);
+        setContentView(R.layout.activity_pupdate_response);
 
-//        button = (Button) findViewById(R.id.p_btn_editResponse_btn);
-//        button.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View v){
-//                openRequestServices();
-//            }
-//        });
+        //
+//        Spinner n_edit_request_services_spinner = findViewById(R.id.n_edit_request_services_spinner);
+//        n_edit_request_services_spinner.setOnItemSelectedListener(this);
 
-        textViewDescription = findViewById(R.id.p_tv1_viewResponse_description);
-        textViewLocation = findViewById(R.id.p_tv1_viewResponse_location);
-        textViewTitle = findViewById(R.id.p_tv1_viewResponse_title);
-        textViewCategory = findViewById(R.id.p_tv1_viewResponse_category);
+        textViewDescription = findViewById(R.id.p_et_update_response_description);
+        // textViewLocation = findViewById(R.id.n_edit_request_services_spinner);
+        textViewTitle = findViewById(R.id.p_et_update_response_title);
 
         docRef.get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -62,32 +55,41 @@ public class p_view_response extends AppCompatActivity {
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
                         if(documentSnapshot.exists()){
                             String description = documentSnapshot.getString(KEY_DESCRIPTION);
-                            String location = documentSnapshot.getString(KEY_LOCATION);
-                            String category = documentSnapshot.getString(KEY_CATEGORY);
+                            //             String location = documentSnapshot.getString(KEY_LOCATION);
                             String title = documentSnapshot.getString(KEY_TITLE);
 
                             textViewDescription.setText(description);
-                            textViewLocation.setText(location);
+                            //  textViewLocation.setOnItemSelectedListener(getSelectedItem(location).toString());
                             textViewTitle.setText(title);
-                            textViewCategory.setText(category);
                         }else {
-                            Toast.makeText(p_view_response.this, "Document does not exist",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(p_update_response.this, "Document does not exist",Toast.LENGTH_SHORT).show();
                         }
                     }
                 })
                 .addOnFailureListener(new OnFailureListener(){
                     public void onFailure(Exception e){
-                        Toast.makeText(p_view_response.this, "Error",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(p_update_response.this, "Error",Toast.LENGTH_SHORT).show();
                     }
                 });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this, adapterView.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 
-    public void deleteResponse(View view) {
-        docRef.delete();
+
+    public void updateResponse(View view) {
+        String description = textViewDescription.getText().toString();
+        String title = textViewTitle.getText().toString();
+
+        docRef.update(KEY_DESCRIPTION,description);
+        docRef.update(KEY_TITLE,title);
     }
-//    private void openRequestServices() {
-//        Intent intent = new Intent(this, p_edit_response.class);
-//        startActivity(intent);
-//    }
+
 }
