9110771d6e18fc5fcc098f5ef9413f08b38d4000
Rasanga Lakshith <64822421+rasangalk@users.noreply.github.com>
2021-10-08 21:40:37 +0530

    Update OrderModel.java

--- app/src/main/java/com/example/handyman_android_app/Model/OrderModel.java
+++ app/src/main/java/com/example/handyman_android_app/Model/OrderModel.java
@@ -1,2 +1,74 @@
-package com.example.handyman_android_app.Model;public class OrderModel {
+package com.example.handyman_android_app.Model;
+
+public class OrderModel {
+
+    private String category, date, description, location, orderId, userID, phone;
+
+
+    public OrderModel(String category, String date, String description, String location, String orderId, String phone, String userID) {
+        this.category = category;
+        this.date = date;
+        this.description = description;
+        this.location = location;
+        this.orderId = orderId;
+        this.userID = userID;
+        this.phone = phone;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
+
